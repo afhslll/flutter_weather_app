7d4ad06e120b18f6eac7516201e7f621010a0bc6
Afifah Salimi <afifahsalimi@gmail.com>
2020-10-14 00:13:14 +0800

    Set height multiplier

--- lib/ui/view/home/home_background_painter.dart
+++ lib/ui/view/home/home_background_painter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/core/utils/utils.dart';
 import 'package:flutter_weather_app/ui/shared/theme_color.dart';
 
 class HomeBackgroundPainter extends StatelessWidget {
@@ -19,8 +20,10 @@ class HomeBackgroundPainter extends StatelessWidget {
 class CurveBackground extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    final heightMultiplier = getHeightMultiplier(size.height);
+
     final Paint paint1 = Paint()..color = ThemeColor.yellowCream;
-    final height1 = (size.height / 2) + 100;
+    final height1 = (size.height / 2) + (100 * heightMultiplier);
     final Path path1 = Path();
     path1.moveTo(40, 0);
     path1.conicTo(50, height1, size.width, height1, 0.4);
@@ -28,11 +31,11 @@ class CurveBackground extends CustomPainter {
     path1.close();
     canvas.drawPath(path1, paint1);
 
-    final Paint paint2 = Paint()..color = ThemeColor.darkblue;
+    final Paint paint2 = Paint()..color = ThemeColor.darkBlue;
     final Path path2 = Path();
-    path2.lineTo(0, (size.height / 2) + 60);
-    path2.cubicTo(90, (size.height / 2) + 70, 90, (size.height / 2) - 70,
-        size.width / 2, (size.height / 2) - 100);
+    path2.lineTo(0, (size.height / 2) + (70 * heightMultiplier));
+    path2.cubicTo(90, (size.height / 2) + (70 * heightMultiplier), 90,
+        (size.height / 2) - 70, size.width / 2, (size.height / 2) - 105);
     path2.cubicTo(
         (size.width / 2) + 90,
         (size.height / 2) - 130,
